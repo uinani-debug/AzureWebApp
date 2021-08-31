2324e5c24822ce1235daeac113e37392b813f33a
urmila inani <urmila.inani@gmail.com>
2021-08-31 21:21:45 +0100

    ok

---
+++ AzureWebApps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureWebApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
